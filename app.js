bcb161c194f53fcdf94741c4599bbe8faf4af531
Jensen <cstblog@163.com>
2016-04-12 20:07:01 +0800

    update to chapter 5

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 var settings = require('./settings');
+var flash = require('connect-flash');
+var multer = require('multer');
+var upload = multer({dest:'./public/images'});
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,6 +27,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  var file = req.file;
+    console.log(file);
+});
+
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+app.use(session({
+	secret:settings.cookieSecret,
+	key:settings.db,
+	cookie:{maxAge:1000*60*60*24*30},
+	store:new MongoStore({
+		db:settings.db,
+		host:settings.host,
+		port:settings.port
+	})
+}));
 //app.use('/', routes);
 //app.use('/users', users);
 routes(app);
@@ -58,18 +83,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-
-app.use(session({
-	secret:settings.cookieSecret,
-	key:settings.db,
-	cookie:{maxAge:1000*60*60*24*30},
-	store:new MongoStore({
-		db:settings.db,
-		host:settings.host,
-		port:settings.port
-	})
-}));
+app.use(function(req, res, next){
+  res.locals.user = req.session.user;
+    next();
+});
 
 module.exports = app;
