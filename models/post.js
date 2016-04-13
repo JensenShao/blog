618c64730767a497b906bbd23e6e32bb1af5b1d9
v-zhesh <v-zhesh@microsoft.com>
2016-04-13 15:55:07 +0800

    Add comments feature

--- models/post.js
+++ models/post.js
@@ -26,7 +26,8 @@ Post.prototype.save = function(callback) {
       name: this.name,
       time: time,
       title: this.title,
-      post: this.post
+      post: this.post,
+      comments:[]
   };
   //打开数据库
   mongodb.open(function (err, db) {
@@ -110,7 +111,13 @@ Post.getOne = function(name, day, title, callback){
 					return callback(err);
 				}
 				
-				doc.post=markdown.toHTML(doc.post);
+//				doc.post=markdown.toHTML(doc.post);
+				if(doc){
+					doc.post =markdown.toHTML(doc.post);
+					doc.comments.forEach(function(comment){
+						comment.content = markdown.toHTML(comment.content);
+					})
+				}
 				callback(null,doc);
 			});	
 			
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var crypto = require('crypto'),
     User = require('../models/user.js'),
-    Post = require('../models/post.js');
+    Post = require('../models/post.js'),
+    Comment = require('../models/comment.js');
 
 module.exports = function(app) {
   app.get('/', function (req, res) {
@@ -180,6 +181,30 @@ module.exports = function(app) {
   			error:req.flash('error').toString()
   		});
   	});
+  });
+  
+  app.post('/u/:name/:day/:title',function(req,res,next){
+  	var date = new Date(),
+  	time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
+             date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+    
+    var comment = {
+    	name:req.body.name,
+    	email:req.body.email,
+    	website:req.body.website,
+    	time:time,
+    	content:req.body.content
+    };
+    var newcomment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+    newcomment.save(function(err){
+    	if(err){
+    		req.flash('error',err);
+    		return res.redirect('back');
+    	}
+    	
+    	req.flash('success','Comment successfully');
+    	res.redirect('back');
+    });
   });
   
   app.get('/edit/:name/:day/:title',checkLogin);
